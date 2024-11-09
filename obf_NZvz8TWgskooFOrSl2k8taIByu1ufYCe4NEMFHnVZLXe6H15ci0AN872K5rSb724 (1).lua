29c787d2de388acae85c271c0402d267d370c0e2
beamedbysouth <164782091+beamedbysouth@users.noreply.github.com>
2024-11-09 19:23:28 +0000

    Update obf_NZvz8TWgskooFOrSl2k8taIByu1ufYCe4NEMFHnVZLXe6H15ci0AN872K5rSb724 (1).lua

--- obf_NZvz8TWgskooFOrSl2k8taIByu1ufYCe4NEMFHnVZLXe6H15ci0AN872K5rSb724 (1).lua
+++ obf_NZvz8TWgskooFOrSl2k8taIByu1ufYCe4NEMFHnVZLXe6H15ci0AN872K5rSb724 (1).lua
@@ -1,1 +1,82 @@
-print("Hello World!")
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+]=]
+
+-- Instances: 6 | Scripts: 0 | Modules: 0 | Tags: 0
+local G2L = {};
+
+-- StarterGui.ScreenGui
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.ScreenGui.Frame
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(158, 0, 4);
+G2L["2"]["Size"] = UDim2.new(0, 357, 0, 97);
+G2L["2"]["Position"] = UDim2.new(0.7492, 0, 0.85452, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"]["BackgroundTransparency"] = 0.65;
+
+
+-- StarterGui.ScreenGui.Frame.Logo
+G2L["3"] = Instance.new("TextLabel", G2L["2"]);
+G2L["3"]["BorderSizePixel"] = 0;
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 0, 5);
+G2L["3"]["TextSize"] = 70;
+G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["3"]["TextColor3"] = Color3.fromRGB(255, 0, 5);
+G2L["3"]["BackgroundTransparency"] = 1;
+G2L["3"]["Size"] = UDim2.new(0, 93, 0, 93);
+G2L["3"]["BorderColor3"] = Color3.fromRGB(255, 0, 0);
+G2L["3"]["Text"] = [[L]];
+G2L["3"]["Name"] = [[Logo]];
+
+
+-- StarterGui.ScreenGui.Frame.Time
+G2L["4"] = Instance.new("TextLabel", G2L["2"]);
+G2L["4"]["BorderSizePixel"] = 0;
+G2L["4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["4"]["TextSize"] = 18;
+G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["4"]["TextColor3"] = Color3.fromRGB(255, 0, 0);
+G2L["4"]["BackgroundTransparency"] = 1;
+G2L["4"]["Size"] = UDim2.new(0, 264, 0, 50);
+G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["Text"] = [[Loaded in 1.56 seconds!]];
+G2L["4"]["Name"] = [[Time]];
+G2L["4"]["Position"] = UDim2.new(0.22409, 0, 0.4433, 0);
+
+
+-- StarterGui.ScreenGui.Frame.Welcome
+G2L["5"] = Instance.new("TextLabel", G2L["2"]);
+G2L["5"]["BorderSizePixel"] = 0;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["TextSize"] = 25;
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/GothamSSm.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["BackgroundTransparency"] = 1;
+G2L["5"]["Size"] = UDim2.new(0, 264, 0, 50);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Text"] = [[Welcome Buyer!]];
+G2L["5"]["Name"] = [[Welcome]];
+G2L["5"]["Position"] = UDim2.new(0.22409, 0, 0, 0);
+
+
+-- StarterGui.ScreenGui.Frame
+G2L["6"] = Instance.new("Frame", G2L["1"]);
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(158, 0, 4);
+G2L["6"]["Size"] = UDim2.new(0, 357, 0, -4);
+G2L["6"]["Position"] = UDim2.new(0.7492, 0, 0.97311, 0);
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+
+
+
+return G2L["1"], require;
